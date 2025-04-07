f3e3d8377a528520b61e522fe539694a62b3b704
Vladislav-Romanov27 <vladislav.romanov.qubership@gmail.com>
2025-04-07 14:57:43 +0300

    test(ci): added docker test for nifi in tls mode (#37)
    * feat: added test for nifi in tls mode
    
    * feat: fix error in run-nifi-tests-tls
    
    * feat: fix error
    
    * feat: increase waitTimeout for tls
    
    * feat: removed extra comment
    
    * feat: fix hostName
    
    * feat: change collection file name
    
    * feat: fix collection-name
    
    * feat: fix errors
    
    * feat: fix error

--- .github/workflows/sh/nifi-lib.sh
+++ .github/workflows/sh/nifi-lib.sh
@@ -1,45 +1,55 @@
 #!/bin/bash -e
 
-wait_for_nifi(){
-    local isTls="$1"
-    local timeout="$2"
-    local nifiHost="$3"
-    local nifiPort="$4"
-    
+wait_for_service(){
+    local serviceHost="$1"
+    local servicePort="$2"
+    local apiUrlToCheck="$3"
+    local timeout="$4"
+    local isTls="$5"
+    local tlsAdditionalCAs="$6"
+    local tlsClientKeystore="$7"
+    local tlsClientPassword="$8"
+
     if [ -z "$timeout" ]; then
         echo "Using default timeout = 180 seconds"
         timeout=180
     fi
-    if [ -z "$nifiHost" ]; then
-        echo "Using default nifi host = 127.0.0.1"
-        nifiHost="127.0.0.1"
+    if [ -z "$serviceHost" ]; then
+        echo "Using default host = 127.0.0.1"
+        serviceHost="127.0.0.1"
     fi
-    if [ -z "$nifiPort" ]; then
-        echo "Using default nifi port = 8080"
-        nifiPort="8080"
+    if [ -z "$servicePort" ]; then
+        echo "Using default port = 8080"
+        servicePort="8080"
     fi
-    
-    nifiUrl=""
-    #TODO: additionalArguments=""
+    if [ -z "$apiUrlToCheck" ]; then
+        echo "Using default apiUrlToCheck = /nifi-api/controller/config"
+        apiUrlToCheck='/nifi-api/controller/config'
+    fi
+
+    serviceUrl=""
+    tlsArgs=""
     if [ "${isTls}" == "true" ]; then
         echo "Using TLS mode..."
-        echo "Waiting for nifi to be available on port 8080 with timeout = $timeout"
-        nifiUrl="https://$nifiHost:$nifiPort/nifi-api/controller/config"
+        echo "Waiting for service to be available on port 8080 with timeout = $timeout"
+        serviceUrl="https://$serviceHost:$servicePort$apiUrlToCheck"
+        echo "Client keystore: $tlsClientKeystore (p12), ca cert = $tlsAdditionalCAs"
+        tlsArgs=" --cert '$tlsClientKeystore:$tlsClientPassword' --cert-type P12 --cacert $tlsAdditionalCAs"
     else
         echo "Using plain mode..."
-        echo "Waiting for nifi to be available on port 8080 with timeout = $timeout"
-        nifiUrl="http://$nifiHost:$nifiPort/nifi-api/controller/config"
+        echo "Waiting for service to be available on port 8080 with timeout = $timeout"
+        serviceUrl="http://$serviceHost:$servicePort$apiUrlToCheck"
     fi
-    
+
     startTime=$(date +%s)
     endTime=$((startTime+timeout))
     remainingTime="$timeout"
     res=1
     while [ "$res" != "0" ]; do
-        echo "Waiting for nifi to be available under URL = $nifiUrl, remaining time = $remainingTime"
+        echo "Waiting for service to be available under URL = $serviceUrl, remaining time = $remainingTime"
         res=0
         resp_code=""
-        resp_code=$(curl -sS -w '%{response_code}' -o ./temp-resp.json --connect-timeout 5 --max-time 10 "$nifiUrl") || { res="$?"; echo "Failed to call NiFi API, continue waiting..."; }
+        resp_code=$(eval curl -sS -w '%{response_code}' -o ./temp-resp.json --connect-timeout 5 --max-time 10 "$tlsArgs" "$serviceUrl") || { res="$?"; echo "Failed to call service API, continue waiting..."; }
         if [ "$res" == "0" ]; then
             if [ "$resp_code" != '200' ]; then
                 echo "Got response with code = $resp_code and body: "
@@ -55,7 +65,7 @@ wait_for_nifi(){
         fi
         sleep 2
     done
-    echo "Wait finished successfully. NiFi is available."
+    echo "Wait finished successfully. Service is available."
 }
 
 generate_random_hex_password(){
@@ -96,6 +106,7 @@ test_log_level(){
     local targetPkg="$1"
     local targetLevel="$2"
     local resultsDir="$3"
+    local containerName="$4"
     resultsPath="./test-results/$resultsDir"
     echo "Testing Consul logging parameters configuration for package = $targetPkg, level = $targetLevel"
     echo "Results path = $resultsPath"
@@ -104,7 +115,7 @@ test_log_level(){
     echo "Waiting 20 seconds..."
     sleep 20
     echo "Copying logback.xml..."
-    docker cp local-nifi-plain:/opt/nifi/nifi-current/conf/logback.xml "$resultsPath/logback.xml"
+    docker cp "$containerName":/opt/nifi/nifi-current/conf/logback.xml "$resultsPath/logback.xml"
     res="0"
     grep "$targetPkg" "$resultsPath/logback.xml" | grep 'logger' | grep "$targetLevel" || res="1"
     if [ "$res" == "0" ]; then
@@ -117,7 +128,7 @@ test_log_level(){
 
 prepare_sens_key(){
     echo "Generating temporary sensitive key..."
-    NIFI_SENSITIVE_KEY=$(generate_random_hex_password 14)
+    NIFI_SENSITIVE_KEY=$(generate_random_hex_password 12 4)
     export NIFI_SENSITIVE_KEY
     echo "$NIFI_SENSITIVE_KEY" > ./nifi-sens-key.tmp
 }
@@ -136,11 +147,16 @@ wait_nifi_container(){
     local useTls="$5"
     local containerName="$6"
     local resultsDir="$7"
+    local caCert="$8"
+    local clientKeystore="$9"
+    local clientPassword="${10}"
+    local apiUrl="/nifi-api/controller/config"
     echo "Sleep for $initialWait seconds..."
     sleep "$initialWait"
-    echo "Waiting for nifi container on $hostName:$portNum (TLS = $useTls, containerName = $containerName) to start..."
+    echo "Waiting for nifi on $hostName:$portNum (TLS = $useTls, container = $containerName, apiUrl=$apiUrl) to start..."
     wait_success="1"
-    wait_for_nifi "$useTls" "$waitTimeout" "$hostName" "$portNum" || wait_success="0"
+    wait_for_service "$hostName" "$portNum" "$apiUrl" "$waitTimeout" "$useTls" \
+      "$caCert" "$clientKeystore" "$clientPassword" || wait_success="0"
     if [ "$wait_success" == '0' ]; then
         echo "Wait failed, nifi not available. Last 500 lines of logs for container:"
         echo "resultsDir=$resultsDir"
@@ -149,4 +165,51 @@ wait_nifi_container(){
         echo "Wait failed, nifi not available" > "./test-results/$resultsDir/failed_nifi_wait.lst"
         mv ./nifi_log_tmp.lst "./test-results/$resultsDir/nifi_log_after_wait.log"
     fi
+}
+
+wait_nifi_reg_container(){
+    local hostName="$1"
+    local portNum="$2"
+    local containerName="$3"
+    local useTls="$4"
+    local initialWait="$5"
+    local waitTimeout="$6"
+    local resultsDir="$7"
+    local caCert="$8"
+    local clientKeystore="$9"
+    local clientPassword="${10}"
+    local apiUrl='/nifi-registry-api/config'
+    echo "Sleep for $initialWait seconds..."
+    sleep "$initialWait"
+    echo "Waiting for nifi registry on $hostName:$portNum (TLS = $useTls, url = $apiUrl) to start..."
+    wait_success="1"
+    wait_for_service "$hostName" "$portNum" "$apiUrl" "$waitTimeout" "$useTls" \
+      "$caCert" "$clientKeystore" "$clientPassword" || wait_success="0"
+    if [ "$wait_success" == '0' ]; then
+        echo "Wait failed, nifi registry not available. Last 500 lines of logs for container:"
+        echo "resultsDir=$resultsDir"
+        docker logs -n 500 "$containerName" > ./nifi_registry_log_tmp.lst
+        cat ./nifi_registry_log_tmp.lst
+        echo "Wait failed, nifi registry not available" > "./test-results/$resultsDir/failed_nifi_registry_wait.lst"
+        mv ./nifi_registry_log_tmp.lst "./test-results/$resultsDir/nifi_registry_log_after_wait.log"
+    fi
+}
+
+generate_tls_passwords(){
+    echo "Generating passwords..."
+    TRUSTSTORE_PASSWORD=$(generate_random_hex_password 8 4)
+    KEYSTORE_PASSWORD_NIFI=$(generate_random_hex_password 8 4)
+    KEYSTORE_PASSWORD_NIFI_REG=$(generate_random_hex_password 8 4)
+    export TRUSTSTORE_PASSWORD
+    export KEYSTORE_PASSWORD_NIFI
+    export KEYSTORE_PASSWORD_NIFI_REG
+}
+
+create_docker_env_file(){
+    echo "Generating environment file for docker-compose..."
+    echo "TRUSTSTORE_PASSWORD=$TRUSTSTORE_PASSWORD" > ./docker.env
+    echo "KEYSTORE_PASSWORD_NIFI=$KEYSTORE_PASSWORD_NIFI" >> ./docker.env
+    echo "KEYSTORE_PASSWORD_NIFI_REG=$KEYSTORE_PASSWORD_NIFI_REG" >> ./docker.env
+    gitDir="$(pwd)"
+    echo "BASE_DIR=$gitDir" >> ./docker.env
 }
